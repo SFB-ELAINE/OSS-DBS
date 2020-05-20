f638bba9ae0b79113e1407bb050752798470e957
Konstantin Butenko <35195806+Kinway25@users.noreply.github.com>
2020-05-20 18:49:24 +0000

    Update Math_module_floating_MPI.py

--- OSS_platform/Math_module_floating_MPI.py
+++ OSS_platform/Math_module_floating_MPI.py
@@ -169,7 +169,14 @@ def get_field_on_points(phi_r,phi_i,c_c,J_r,J_i):
 def compute_field_with_superposition(mesh_sol,Domains,subdomains,boundaries_sol,kappa,Field_calc_param):
         
     start_math=tm.time()
-    
+
+    from Math_module_hybrid import choose_solver_for_me
+    if Field_calc_param.Solver_type=='Default':
+        Solver_type=choose_solver_for_me(Field_calc_param.EQS_mode,Domains.Float_contacts)    #choses solver basing on the Laplace formulation and whether the floating conductors are used
+    else:
+        Solver_type=Field_calc_param.Solver_type      # just get the solver directly
+    #IMPORTANT: for get_field_with_floats when solving EQS we always use direct solver MUMPS for stability issues (multiple floating conductors)
+        
     if Field_calc_param.element_order==1 and MPI.comm_world.rank==1:
         print("Selected element_order (1st) is too low for current-controlled stimulation, increasing to 2nd")
         Field_calc_param.element_order=2   
@@ -197,9 +204,9 @@ def compute_field_with_superposition(mesh_sol,Domains,subdomains,boundaries_sol,
                 # to solve "one active contact (i) vs ground" system, get potentials on the rest of the active contacts (which are put to floating condcutors), get current on the active contact
                 from Math_module_hybrid_floating import get_field_with_floats
                 if Field_calc_param.EQS_mode == 'EQS':
-                    phi_r_floating[glob_counter,:],phi_i_floating[glob_counter,:],J_real_current_contacts[glob_counter],J_im_current_contacts[glob_counter]=get_field_with_floats(mesh_sol,i,Domains,subdomains,boundaries_sol,Field_calc_param.default_material,Field_calc_param.element_order,Field_calc_param.anisotropy,Field_calc_param.frequenc,Field_calc_param.EQS_mode,calc_with_MPI=True,kappa=kappa)
+                    phi_r_floating[glob_counter,:],phi_i_floating[glob_counter,:],J_real_current_contacts[glob_counter],J_im_current_contacts[glob_counter]=get_field_with_floats(mesh_sol,i,Domains,subdomains,boundaries_sol,Field_calc_param.default_material,Field_calc_param.element_order,Field_calc_param.anisotropy,Field_calc_param.frequenc,Field_calc_param.EQS_mode,Solver_type,calc_with_MPI=True,kappa=kappa)
                 else:
-                    phi_r_floating[glob_counter,:],__,J_real_current_contacts[glob_counter],__=get_field_with_floats(mesh_sol,i,Domains,subdomains,boundaries_sol,Field_calc_param.default_material,Field_calc_param.element_order,Field_calc_param.anisotropy,Field_calc_param.frequenc,Field_calc_param.EQS_mode,calc_with_MPI=True,kappa=kappa)
+                    phi_r_floating[glob_counter,:],__,J_real_current_contacts[glob_counter],__=get_field_with_floats(mesh_sol,i,Domains,subdomains,boundaries_sol,Field_calc_param.default_material,Field_calc_param.element_order,Field_calc_param.anisotropy,Field_calc_param.frequenc,Field_calc_param.EQS_mode,Solver_type,calc_with_MPI=True,kappa=kappa)
 
                 fl_ind[glob_counter,:]=fl_contacts_rel_ind[np.arange(len(fl_contacts_rel_ind))!=glob_counter]   # if three current contacts, it will store [[1,2][0,2],[0,1]]
                 contact_amplitude[glob_counter]=Domains.fi[i]
@@ -246,7 +253,7 @@ def compute_field_with_superposition(mesh_sol,Domains,subdomains,boundaries_sol,
     
     #the results are stored in h5 file, check get_solutions above
     from Math_module_hybrid_floating import get_field_with_scaled_BC
-    get_field_with_scaled_BC(mesh_sol,Domains,scaled_phi,subdomains,boundaries_sol,Field_calc_param.default_material,Field_calc_param.element_order,Field_calc_param.EQS_mode,Field_calc_param.anisotropy,Field_calc_param.frequenc,calc_with_MPI=True,kappa=kappa)
+    get_field_with_scaled_BC(mesh_sol,Domains,scaled_phi,subdomains,boundaries_sol,Field_calc_param.default_material,Field_calc_param.element_order,Field_calc_param.EQS_mode,Field_calc_param.anisotropy,Field_calc_param.frequenc,Solver_type,calc_with_MPI=True,kappa=kappa)
     
     minutes=int((tm.time() - start_math)/60)
     secnds=int(tm.time() - start_math)-minutes*60
