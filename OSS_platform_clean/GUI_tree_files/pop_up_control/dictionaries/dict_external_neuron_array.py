7ef9158637fd1712e1691b10af0a8c48d26efd4d
Konstantin <konstantin.butenko@uni-rostock.de>
2021-02-23 11:09:27 +0100

    Axon_to_files statt Points_to_files

---
+++ OSS_platform_clean/GUI_tree_files/pop_up_control/dictionaries/dict_external_neuron_array.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 28 08:34:11 2018
+
+@author: trieu,butenko
+"""
+import numpy as np
+
+d = {
+    'Global_rot': 1,
+    'x_seed': 2.5781,
+    'y_seed': -2.6821,
+    'z_seed': -0.7,
+    'x_steps': 9,
+    'y_steps': 0,
+    'z_steps': 9,
+    'x_step': 0.2,
+    'y_step': 0.2,
+    'z_step': 0.2,
+    'alpha_array_glob': [0],
+    'beta_array_glob': [0],
+    'gamma_array_glob': [0],
+    }
